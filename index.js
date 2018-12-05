7b4f8b82d3628c8dbc5bd9ecc1eb48d4e78dd003
Kumar Abhinav <kmr.abhinav@gmail.com>
2018-12-05 17:16:42 +0000

    Initial

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function isNullOrEmpty(input) {
+
+
+
+    // Returns true if the input is either undefined, null, or empty, false otherwise
+
+
+
+    return (input === undefined || input === null || input === '');
+
+
+
+}
+module.exports = isNullOrEmpty;
